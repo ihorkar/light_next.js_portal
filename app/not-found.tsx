d738244aab0102261b459eb153e47c79e21ea474
ihorkar <ihorkarpiuk0709@gmail.com>
2023-08-14 13:30:48 +0100

    added the not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import DefaultHeader from '@/components/ui/headers/DefaultHeader';
+
+export default async function Home() {
+      
+  return (
+      <div>
+        <DefaultHeader 
+          Headline = 'Oops! Seems you are lost'
+          Description = 'This is a placeholder'
+          subHeadline = 'This is a placeholder'      
+        />
+      </div>
+  )
+}
