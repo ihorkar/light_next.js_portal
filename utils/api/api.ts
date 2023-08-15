10bc54c37b9f1ed11f324d1b9e8a8a7c60450b78
Kees Diekmann <kees@briggsandwalker.com>
2023-08-15 14:15:21 +0200

    Catch signout & show resultId

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { AxiosResponse } from "axios";
+import { signOut } from "next-auth/react";
 
 export const BASE_URL = process.env.DATABACKEND_URL;
 
@@ -10,18 +11,36 @@ const API = {
         return new Promise((resolve, reject) => {
             axios.get('/organisations')
                 .then(response => resolve(response))
+                .catch(function (error) {
+                    if (error.response.status === 401) {
+                      signOut();
+                    }
+                  }
+                )
         })
     },
     getOrganisationsByUser: (): Promise<AxiosResponse> => {
         return new Promise((resolve, reject) => {
             axios.get('/userorganisations')
                 .then(response => resolve(response))
+                .catch(function (error) {
+                    if (error.response.status === 401) {
+                      signOut();
+                    }
+                  }
+                )
         })
     },
     getResultsByOrganisation: (slug: string): Promise<AxiosResponse> => {
         return new Promise((resolve, reject) => {
             axios.get(`/results/${slug}`)
                 .then(response => resolve(response))
+                .catch(function (error) {
+                    if (error.response.status === 401) {
+                      signOut();
+                    }
+                  }
+                )
         })
     }
 }
