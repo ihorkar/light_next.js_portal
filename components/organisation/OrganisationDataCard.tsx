7a285c57ea293a380861dee0ab39db9d51043a15
ihorkar <ihorkarpiuk0709@gmail.com>
2023-08-23 13:34:11 +0100

    added the payment and contact details

--- components/organisation/OrganisationDataCard.tsx
+++ components/organisation/OrganisationDataCard.tsx
@@ -21,15 +21,32 @@ export interface Address {
     countrycode?: string;
 }
 
+export interface Payment {
+    type?: string;
+    SEPACreditorIdentifier?: string;
+}
+
+export interface ContactDetails {
+    ContactEmail?: string;
+    ContactPhone?: string;
+}
+
+export interface IsModalsOpen {
+    name: boolean;
+    address: boolean;
+    payment: boolean;
+    contactDetails: boolean;
+}
 
 const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
     const router = useRouter();
     const session = useSession();
-    const [ organisationData, setOrganisationData ] = useState<any>()
-    const [ isNameModalOpen, setIsNameModalOpen ] = useState<boolean>(false);
-    const [ isAddressModalOpen, setIsAddressModalOpen ] = useState<boolean>(false);
+    const [ organisationData, setOrganisationData ] = useState<any>();
+    const [ isModalsOpen, setIsModalsOpen ] = useState<IsModalsOpen>({name: false, address: false, payment: false, contactDetails: false})
+    const [ name, setName ] = useState<string>("");
     const [ address, setAddress ] = useState<Address | undefined>();
-    const [ name, setName ] = useState<string>("")
+    const [ contactDetails, setContactDetails ] = useState<ContactDetails | undefined>();
+    const [ payment, setPayment ] = useState<Payment | undefined>();
 
     useEffect(() => {
         //@ts-ignore
@@ -44,6 +61,8 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
             setOrganisationData(response.data)
             setName(response.data.name)
             setAddress(response.data.address)
+            setContactDetails(response.data.contactDetails)
+            setPayment(response.data.payment)
           }
         })
         .catch(error => {
@@ -51,42 +70,45 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
         })
     }
 
-    const handleEditUserName = useCallback(() => {
-        API.updateOrganisation(organisationId, {name: name})
+    const handleEditOrganisationData = useCallback((modalName: string, data: any) => () => {
+        API.updateOrganisation(organisationId, {[modalName]: data})
             .then(response => {
+                console.log(response, "response")
                 if(response.status === 200) {
-                    setIsNameModalOpen(false)
+                    let closeModal = closeModals(modalName)
+                    closeModal();
                     handleGetOrganisationInfo()
                 }
             })
-            .catch(error => console.log("Error while updating the organisation name", error))
-    }, [name])
+            .catch(error => console.log("Error while updating the organisation", error))
+    }, [name, address, payment, contactDetails])
 
-    const handleEditUserAddress = useCallback(() => {
-        API.updateOrganisation(organisationId, {address: address})
-            .then(response => {
-                if(response.status === 200) {
-                    setIsAddressModalOpen(false)
-                    handleGetOrganisationInfo()
-                }
-            })
-            .catch(error => console.log("Error while updating the organisation address", error))
-    }, [address])
-
-    const openEditNameModal = useCallback(() => {
-        setIsNameModalOpen(true)
+    const openModals = useCallback((name: string) => () => {
+        setIsModalsOpen((prev) => ({...prev, [name]: true}))
     }, [])
 
-    const openEditAddressModal = useCallback(() => {
-        setIsAddressModalOpen(true)
+    const closeModals = useCallback((name: string) => () => {
+        console.log(name, "modal name")
+        setIsModalsOpen((prev) => ({...prev, [name]: false}))
     }, [])
 
-    const handleAddressChange =  useCallback((field: string) => (e: ChangeEvent<HTMLInputElement>) => {
-        setAddress((prevState) => ({...prevState, [field]: e.target.value}))
-    }, [])
-
-    const handleNameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value)
+    const modalDataChange = useCallback((modalName: string, field: string) => (e: ChangeEvent<HTMLInputElement>) => {
+        switch (modalName) {
+            case "name":
+                setName(e.target.value)
+                break;
+            case "address":
+                setAddress((prevState) => ({...prevState, [field]: e.target.value}))
+                break;
+            case "payment":
+                setPayment((prevState) => ({...prevState, [field]: e.target.value}))
+                break;
+            case "contactDetails":
+                setContactDetails((prevState) => ({...prevState, [field]: e.target.value}))
+                break;
+            default:
+                break;
+        }
     }, [])
 
     return (
@@ -102,7 +124,7 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                     {
                         label: 'Full Name',
                         value: organisationData.name,
-                        onUpdate: openEditNameModal
+                        onUpdate: openModals('name')
                     },
                     {
                         label: 'Address',
@@ -114,17 +136,38 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                 <div>{`Country code: ${organisationData.address.countrycode ? organisationData.address.countrycode : ""}`}</div>
                             </>
                         ) ,
-                        onUpdate: openEditAddressModal
+                        onUpdate: openModals('address')
+                    },
+                    {
+                        label: 'Payment',
+                        value: organisationData.payment && (
+                            <>
+                                <div>{`type: ${organisationData.payment.type ? organisationData.payment.type : ""}`}</div>
+                                <div>{`SEPA Creditor Identifier: ${organisationData.payment.SEPACreditorIdentifier ? organisationData.payment.SEPACreditorIdentifier : ""}`}</div>
+                            </>
+                        ),
+                        onUpdate: openModals('payment')
+                    },
+                    {
+                        label: 'Contact Details',
+                        value:  organisationData.contactDetails && (
+                            <>
+                                <div>{`Contact Email: ${organisationData.contactDetails.ContactEmail ? organisationData.contactDetails.ContactEmail : ""}`}</div>
+                                <div>{`Phone number: ${organisationData.contactDetails.ContactPhone ? organisationData.contactDetails.ContactPhone : ""}`}</div>
+                            </>
+                        ),
+                        onUpdate: openModals('contactDetails')
                     }
                 ]}
             />}
+            {/* Edit name modal */}
             <Modal 
-                visible={isNameModalOpen} 
+                visible={isModalsOpen.name} 
                 title="Edit user name"
                 ok_text="Update"
                 cancel_text="Cancel"
-                onCancelClick={() => setIsNameModalOpen(false)} 
-                onOkClick={handleEditUserName}
+                onCancelClick={closeModals('name')} 
+                onOkClick={handleEditOrganisationData('name', name)}
                 >      
                     <div className="mt-2 pl-10 space-y-8 pb-12 sm:space-y-0 sm:pb-0">
                         <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
@@ -137,21 +180,21 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                     id="username"
                                     autoComplete="username"
                                     placeholder="username"
-                                    onChange={handleNameChange}
+                                    onChange={modalDataChange("name", "")}
                                     value={name}
                                 />
                             </div>
                         </div>
                     </div>
             </Modal> 
-
+            {/* Edit Address modal */}
             <Modal 
-                visible={isAddressModalOpen} 
+                visible={isModalsOpen.address} 
                 title="Edit user address"
                 ok_text="Update"
                 cancel_text="Cancel"
-                onCancelClick={() => setIsAddressModalOpen(false)} 
-                onOkClick={handleEditUserAddress}
+                onCancelClick={closeModals('address')} 
+                onOkClick={handleEditOrganisationData('address', address)}
                 >      
                     <div className="mt-2 pl-10 space-y-8 pb-12 sm:space-y-0 sm:pb-0">
                         <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
@@ -164,7 +207,7 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                     id="street"
                                     autoComplete="street"
                                     placeholder="street"
-                                    onChange={handleAddressChange("street")}
+                                    onChange={modalDataChange("address", "street")}
                                     value={address?.street}
                                 />
                             </div>
@@ -179,7 +222,7 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                     id="postalcode"
                                     autoComplete="postalcode"
                                     placeholder="postalcode"
-                                    onChange={handleAddressChange("postalcode")}
+                                    onChange={modalDataChange("address", "postalcode")}
                                     value={address?.postalcode}
                                 />
                             </div>
@@ -194,7 +237,7 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                 id="city"
                                 autoComplete="city"
                                 placeholder="city"
-                                onChange={handleAddressChange("city")}
+                                onChange={modalDataChange("address", "city")}
                                 value={address?.city}
                             />
                             </div>
@@ -209,9 +252,93 @@ const OrganisationDataCard = ({organisationId}: OrganisationDataCardProps) => {
                                 id="countrycode"
                                 autoComplete="countrycode"
                                 placeholder="countrycode"
-                                onChange={handleAddressChange("countrycode")}
+                                onChange={modalDataChange("address", "countrycode")}
                                 value={address?.countrycode}
                             />
+                            </div>
+                        </div>
+                    </div>
+            </Modal>
+            {/* Edit Payment modal */}
+            <Modal 
+                visible={isModalsOpen.payment} 
+                title="Edit user address"
+                ok_text="Update"
+                cancel_text="Cancel"
+                onCancelClick={closeModals('payment')} 
+                onOkClick={handleEditOrganisationData('payment', payment)}
+                >      
+                    <div className="mt-2 pl-10 space-y-8 pb-12 sm:space-y-0 sm:pb-0">
+                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                            <label htmlFor="type" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                            Type
+                            </label>
+                            <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                <DefaultInput 
+                                    name="type"
+                                    id="type"
+                                    autoComplete="type"
+                                    placeholder="type"
+                                    onChange={modalDataChange("payment", "type")}
+                                    value={payment?.type}
+                                />
+                            </div>
+                        </div>
+                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                            <label htmlFor="SEPACreditorIdentifier" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                            SEPA Creditor Identifier
+                            </label>
+                            <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                <DefaultInput 
+                                    name="SEPACreditorIdentifier"
+                                    id="SEPACreditorIdentifier"
+                                    autoComplete="SEPACreditorIdentifier"
+                                    placeholder="SEPACreditorIdentifier"
+                                    onChange={modalDataChange("payment", "SEPACreditorIdentifier")}
+                                    value={payment?.SEPACreditorIdentifier}
+                                />
+                            </div>
+                        </div>
+                    </div>
+            </Modal>
+            {/* Edit Contact Detail modal */}
+            <Modal 
+                visible={isModalsOpen.contactDetails} 
+                title="Edit contact details"
+                ok_text="Update"
+                cancel_text="Cancel"
+                onCancelClick={closeModals('contactDetails')} 
+                onOkClick={handleEditOrganisationData('contactDetails', contactDetails)}
+                >      
+                    <div className="mt-2 pl-10 space-y-8 pb-12 sm:space-y-0 sm:pb-0">
+                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                            <label htmlFor="ContactEmail" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                            Email
+                            </label>
+                            <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                <DefaultInput 
+                                    name="ContactEmail"
+                                    id="ContactEmail"
+                                    autoComplete="ContactEmail"
+                                    placeholder="ContactEmail"
+                                    onChange={modalDataChange("contactDetails", "ContactEmail")}
+                                    value={contactDetails?.ContactEmail}
+                                />
+                            </div>
+                        </div>
+                        <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                            <label htmlFor="ContactPhone" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                            Phone number
+                            </label>
+                            <div className="mt-2 sm:col-span-2 sm:mt-0">
+                                <DefaultInput 
+                                    name="ContactPhone"
+                                    id="ContactPhone"
+                                    autoComplete="ContactPhone"
+                                    placeholder="ContactPhone"
+                                    onChange={modalDataChange("contactDetails", "ContactPhone")}
+                                    value={contactDetails?.ContactPhone}
+                                />
                             </div>
                         </div>
                     </div>
