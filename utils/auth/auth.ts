68a6c27f22bd46da1622aa48ba8e5203af851acd
ihorkar <ihorkarpiuk0709@gmail.com>
2023-09-15 11:17:26 +0100

    fixed next-auth provider

--- utils/auth/auth.ts
+++ utils/auth/auth.ts
@@ -1,15 +1,15 @@
 import { NextAuthOptions } from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
-// import { JWT } from "next-auth/jwt";
-// import { OAuthConfig } from "next-auth/providers/oauth";
-// import { KeycloakProfile } from "next-auth/providers/keycloak";
+import { JWT } from "next-auth/jwt";
+import { OAuthConfig } from "next-auth/providers/oauth";
+import { KeycloakProfile } from "next-auth/providers/keycloak";
 
-// declare module 'next-auth/jwt' {
-//     interface JWT {
-//       id_token?: string;
-//       provider?: string;
-//     }
-// }
+declare module 'next-auth/jwt' {
+    interface JWT {
+      id_token?: string;
+      provider?: string;
+    }
+}
 
 export const authConfig: NextAuthOptions = {
     secret: String("process.env.NEXTAUTH_SECRET"),
@@ -24,8 +24,8 @@ export const authConfig: NextAuthOptions = {
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
             if (account) {
-                // token.id_token = account.id_token
-                // token.provider = account.provider
+                token.id_token = account.id_token
+                token.provider = account.provider
                 token.accessToken = account.access_token
             }
             return token
@@ -35,18 +35,18 @@ export const authConfig: NextAuthOptions = {
             //@ts-ignore
             session.accessToken = token.accessToken
             //@ts-ignore
-            session.user.identityId = token.sub
+            // session.user.identityId = token.sub
             return session
         }
     },
-    // events: {
-    //   async signOut({ token }: { token: JWT }) {
-    //     if (token.provider === "keycloak") {
-    //       const issuerUrl = (authConfig.providers.find(p => p.id === "keycloak") as OAuthConfig<KeycloakProfile>).options!.issuer!
-    //       const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`)
-    //       logOutUrl.searchParams.set("id_token_hint", token.id_token!)
-    //       await fetch(logOutUrl);
-    //     }
-    //   },
-    // }
+    events: {
+      async signOut({ token }: { token: JWT }) {
+        if (token.provider === "keycloak") {
+          const issuerUrl = (authConfig.providers.find(p => p.id === "keycloak") as OAuthConfig<KeycloakProfile>).options!.issuer!
+          const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`)
+          logOutUrl.searchParams.set("id_token_hint", token.id_token!)
+          await fetch(logOutUrl);
+        }
+      },
+    }
 }
